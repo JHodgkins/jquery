fb671918c95c8d3592f2f9b57e1c3a45466a4022
JHodgkins <jhodgkins78@gmail.com>
2022-04-18 12:41:00 +0000

    code: writing jquery script examples updated

--- writing_first_jquery_script/js/myscript.js
+++ writing_first_jquery_script/js/myscript.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-    
+/**
+ * This will still only add inline styles after document has loaded
+ */
+$(document).ready(function() { 
+    $('h2').css('text-decpration', 'underline'); // underline all h2 elements
+    $('ul').css('border', '1px solid #000');
 });
+
+/**
+ * add css classes to html element instead
+ */
+$(document).ready(function() {
+    // adds class
+    $('h2').addClass('underline');
+    $('ul').addClass('border');
+    // removes class
+    $('h2').removeClass('underline');
+    $('ul').removeClass('border');
+    // add class odd/even to table
+    $('tr:odd').addClass('odd');
+    $('tr:even').addClass('even');
+})
